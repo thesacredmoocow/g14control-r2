cd839ac34d1b04f5ce747e21b63882f9adbae524
thesacredmoocow <54852893+thesacredmoocow@users.noreply.github.com>
2020-09-04 18:25:51 -0400

    update install.bat

--- install.bat
+++ install.bat
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 pip uninstall setuptools
 pip install setuptools==44.0.0
 python setup.py build
-xcopy build\exe.win32-3.8\ G14control\ /E /C
+xcopy build\exe.win32-3.8\ C:\G14control\ /E /C
